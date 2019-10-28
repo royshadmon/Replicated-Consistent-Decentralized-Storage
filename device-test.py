9e7abe801602027a13ff3180c43259d9db7d86ac
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2019-10-28 15:50:42 -0700

    adding comment

--- device-test.py
+++ device-test.py
@@ -7,6 +7,7 @@ BUFFER_SIZE = 2000
 MESSAGE = input("tcpClientB: Enter message/ Enter exit:")
 
 tcpClientB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# set device to port 55432
 tcpClientB.bind((host, 55432))
 print(tcpClientB) 
 tcpClientB.connect((host, port))
