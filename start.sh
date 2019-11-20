d01483762219b68830fd260971e0315bd1399036
Niharika1203 <niharika.belle@gmail.com>
2019-11-20 13:20:48 -0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -5,15 +5,21 @@ read noofservers
 echo Creating $noofservers servers
 # create/start 3 postgres instances
 START=1
-#END=5
+DBLOCATION= "/Users/niharika/Library/Application\Support/Postgres"
+DBLOGLOCATION= "/Users/niharika/Library/ApplicationSupport/Postgres/postgresql.log"
+PORT=5435
 for (( c=$START; c<=$noofservers; c++ ))
 do
 	echo -n "$c "
+
+	initdb -D /Users/niharika/Library/Application\Support/Postgres
+	echo "here"
+	echo "pg_ctl -D $DBLOCATION -o $PORT -l $DBLOGLOCATION start"
+	pg_ctl -D $DBLOCATION -o "$((PORT + 1))" -l $DBLOGLOCATION start
+	#pg_ctl -D /Users/niharika/Library/ApplicationSupport/Postgres -o "-p 5435" -l /Users/niharika/Library/ApplicationSupport/Postgres/postgresql.log stop
 done
 echo
 # start 3 python3 servers each connecting to its own db instnace
-
-# automation of server->server (many-to-many)
 
 # script should run python3 device.py
 
